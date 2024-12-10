a0051732a528ceda9d8f8395e8abe2a974eb0f7e
baalaji03 <rps123a@gmail.com>
2024-12-10 14:44:48 +0530

    10-12-2024 chore: Sent input data

--- database/Config.js
+++ database/Config.js
@@ -2,13 +2,19 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 const mongoDB_URL = process.env.MONGODB_URL;
+
+
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(mongoDB_URL);
+    const connection = await mongoose.connect(mongoDB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("MongoDB Connected successfully");
     return connection;
   } catch (error) {
-    throw new Error({ message: "MongoDB connection Error" });
+    console.error("MongoDB connection Error:", error.message);
+    throw new Error("Failed to connect to MongoDB"); // Throw the error for the caller to handle
   }
 };
 export default connectDB;
